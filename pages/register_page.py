2eae072aaeb8735c902d35594e79beb5fb1d9985
Israel Wasserman <im.vaserman@gmail.com>
2025-06-18 19:09:08 +0300

    Changes in 'register_page.py'

--- pages/register_page.py
+++ pages/register_page.py
@@ -1,9 +1,12 @@
+import random
+import string
 from selenium.common import TimeoutException
 from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
 import requests
+from pages.login_page import LoginPage
 
-class RegisterPage(BasePage):
+class RegisterPage(BasePage, LoginPage):
     NAV_LINKS = (By.XPATH, "//nav/a")
     FIRST_NAME = (By.XPATH, "//input[@id='firstname']")
     LAST_NAME = (By.XPATH, "//input[@id='lastname']")
@@ -19,8 +22,20 @@ class RegisterPage(BasePage):
                 link.click()
                 break
 
-    def get_user_from_api(self):
-        pass
+    def get_users_list_from_api(self):
+        response = requests.get("https://carsphere.onrender.com/get-users")
+        existing_users = response.text
+        return existing_users
+    
+    def generate_new_random_username(self, existing_users):
+        random_username = existing_users[0]
+        while random_username in existing_users:
+            random_username = "Auto_username" + ''.join(random.choices(string.digits, k=3))
+        return random_username
+    
+    def generate_new_random_password(self):
+        password = "1234"
+        return password
 
     def fill_registration_form(self, first_name, last_name, username, password, confirm_password):
         self.find_element(*self.FIRST_NAME).send_keys(first_name)
@@ -29,3 +44,12 @@ class RegisterPage(BasePage):
         self.find_element(*self.PASSWORD).send_keys(password)
         self.find_element(*self.CONFIRM_PASSWORD).send_keys(confirm_password)
         self.find_element(*self.SUBMIT_BUTTON).click()
+
+    def get_alert_danger_message(self):
+        return self.find_element(By.CSS_SELECTOR, ".alert.alert-danger").text
+    
+    def get_alert_mismatch_password_message(self):
+        mismatch_password_alert = self.find_element(By.ID, "confirm_pass").text
+        mismatch_password_alert_expected_color = mismatch_password_alert.value_of_css_property("color")
+        return mismatch_password_alert, mismatch_password_alert_expected_color
+        
